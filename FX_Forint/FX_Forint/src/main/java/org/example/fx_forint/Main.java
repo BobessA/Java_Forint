081ead9ddb3b21bd79e485459f87df3258d505df
BobessA <adria72n@gmail.com>
2024-11-01 09:51:25 +0100

    refactor

--- FX_Forint/FX_Forint/src/main/java/org/example/fx_forint/Main.java
+++ FX_Forint/FX_Forint/src/main/java/org/example/fx_forint/Main.java
@@ -2,6 +2,6 @@ package org.example.fx_forint;
 
 public class Main {
     public static void main(String[] args) {
-        HelloApplication.main(args);
+        MainApplication.main(args);
     }
 }
