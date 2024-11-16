13ad8b6140d376ee9f18ef18eaa74fefa4049080
BobessA <adria72n@gmail.com>
2024-11-16 17:14:36 +0100

    soap kliens

--- FX_Forint/FX_Forint/src/main/java/module-info.java
+++ FX_Forint/FX_Forint/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module org.example.fx_forint {
     requires javafx.fxml;
     requires javafx.controls;
     requires java.sql;
+    requires jakarta.xml.ws;
 
     exports org.example.fx_forint;
     exports org.example.fx_forint.controller;
@@ -12,4 +13,7 @@ module org.example.fx_forint {
     opens org.example.fx_forint.models to javafx.fxml;
     opens org.example.fx_forint.controller to javafx.fxml;
     opens org.example.fx_forint.helper to javafx.fxml;
+
+    opens soap to com.sun.xml.bind, com.sun.xml.ws;
+    //opens org.example.fx_forint to javafx.fxml, com.sun.xml.bind;
 }
---
+++ FX_Forint/FX_Forint/src/main/java/org/example/fx_forint/helper/MNBSoapClientHelper.java
@@ -0,0 +1,8 @@
+package org.example.fx_forint.helper;
+
+
+
+
+public class MNBSoapClientHelper {
+
+}
